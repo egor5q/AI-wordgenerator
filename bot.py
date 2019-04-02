c087422b4c35c95fada4af9229e0b97ec1a582ae
egor5q <31898567+egor5q@users.noreply.github.com>
2019-04-02 13:11:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import threading
 from emoji import emojize
 from telebot import types
 from pymongo import MongoClient
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 
 token = os.environ['TELEGRAM_TOKEN']
@@ -25,6 +26,15 @@ except Exception as e:
     print('Ошибка:\n', traceback.format_exc())
     bot.send_message(441399484, traceback.format_exc())
 
+    
+@bot.message_handler()
+def addword(m):
+    textwords=word_tokenize(i)
+    x=words.find_one({'word':})
+    for ids in x:
+        
+    
+    
 print('7777')
 bot.polling(none_stop=True,timeout=600)
 
