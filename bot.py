0fab5981ea048f41ded70f7a18bf2a46c22e85ff
egor5q <31898567+egor5q@users.noreply.github.com>
2019-04-02 14:03:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ import threading
 from emoji import emojize
 from telebot import types
 from pymongo import MongoClient
-from nltk.tokenize import sent_tokenize, word_tokenize
 
 
 token = os.environ['TELEGRAM_TOKEN']
@@ -15,8 +14,8 @@ bot = telebot.TeleBot(token)
 
 
 client=MongoClient(os.environ['database'])
-db=client.
-users=db.users
+db=client.aiwordgen
+words=db.words
 
 
 try:
@@ -29,9 +28,48 @@ except Exception as e:
     
 @bot.message_handler()
 def addword(m):
-    textwords=word_tokenize(i)
-    x=words.find_one({'word':})
-    for ids in x:
+    if m.from_user.id!=m.chat.id:
+        toupdate={}
+        allword=words.find_one({})
+        textwords=m.text.split(' ')
+        i=0
+        for ids in textwords:
+            currentword=ids
+            if currentword=='&start':
+                currentword='start'
+            if i==0:
+                toupdate.update({'&start':{ids:1}})
+            end=False
+            try:
+                nextword=textwords[i+1]
+            except:
+                nextword='&end'
+                end=True
+            if nextword=='&end' and end==False:
+                nextword='end'
+            if ids not in toupdate:     
+                toupdate.update({currentword:{nextword:1}})
+            else:
+                if nextword not in toupdate[currentword]:
+                    toupdate[currentword][nextword]+=1
+                else:
+                    toupdate[currentword].update({nextword:1})
+            i+=1
+            
+        for ids in toupdate:
+            if ids not in allword['words']:
+                words.update_one({},{'$set':{'words.'+str(ids):toupdate[ids]}})
+            else:
+                for idss in toupdate[ids]:
+                    if idss not in allword['words'][ids]:
+                        words.update_one({},{'$set':{'words.'+str(ids)+'.'+str(idss):allword[ids][idss]}})
+                    else:
+                        words.update_one({},{'$inc':{'words.'+str(ids)+'.'+str(idss):allword[ids][idss]}})
+                        
+        toprint=words.find_one({})
+        print(toprint)
+                                       
+            
         
     
     
